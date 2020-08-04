c7d93ebc0d2a29ccc948a5ef5e8f8a85579ed05f
Aline Duarte Soares <alineds.ds@gmail.com>
2020-08-03 21:21:24 -0300

    Add files via upload

--- ex040.py
+++ ex040.py
@@ -3,8 +3,8 @@ n2 = float(input('Segunda nota: '))
 media = (n1 + n2) / 2
 
 if media < 5:
-    print('Sua média foi {} e você foi REPROVADO'.format(media))
+    print('Sua média foi {:.1f} e você foi REPROVADO'.format(media))
 elif 5 <= media <= 6.9:
-    print('Sua média foi {} e você está em RECUPERAÇÃO'.format(media))
+    print('Sua média foi {:.1f} e você está em RECUPERAÇÃO'.format(media))
 else:
-    print('Sua média foi {} e você foi APROVADO!!'.format(media))
+    print('Sua média foi {:.1f} e você foi APROVADO!!'.format(media))
